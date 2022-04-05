d81870cf47fa859f7d439b0fb8c3a6e13d4b33dc
Quinn Gil <Fyzxs@users.noreply.github.com>
2022-04-04 22:21:45 -0700

    c Add smells as comments

--- Class1.cs
+++ Class1.cs
@@ -1,36 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 
+//Smell - file and namespace names
 namespace expensereport_csharp
 {
+    //DS - Enum
+    //DS - Naming Convention
     public enum ExpenseType
     {
+        //DS - undeclared backing value
         DINNER, BREAKFAST, CAR_RENTAL
     }
-
+    
     public class Expense
     {
+        //DS - DTO
+        //DS - Exposed fields
+        //DS - money as int
         public ExpenseType type;
         public int amount;
     }
 
+    //DS - Could be static
     public class ExpenseReport
     {
+        //DS - Could be static
         public void PrintReport(List<Expense> expenses)
         {
+            //DS - money as int
             int total = 0;
             int mealExpenses = 0;
 
+            //DS - Hard coupling to specific report output
+            //DS - Direct use of DateTime
             Console.WriteLine("Expenses " + DateTime.Now);
 
+            //DS - type behavior spread over 3 sections
             foreach (Expense expense in expenses)
             {
+                
                 if (expense.type == ExpenseType.DINNER || expense.type == ExpenseType.BREAKFAST)
                 {
                     mealExpenses += expense.amount;
                 }
 
                 String expenseName = "";
+                //DS - switch
+                //DS - Hard coding
                 switch (expense.type)
                 {
                     case ExpenseType.DINNER:
@@ -44,12 +60,14 @@ namespace expensereport_csharp
                         break;
                 }
 
+                //DS - Hard Coded ... lots
                 String mealOverExpensesMarker =
                     expense.type == ExpenseType.DINNER && expense.amount > 5000 ||
                     expense.type == ExpenseType.BREAKFAST && expense.amount > 1000
                         ? "X"
                         : " ";
 
+                //DS - coupled writer
                 Console.WriteLine(expenseName + "\t" + expense.amount + "\t" + mealOverExpensesMarker);
 
                 total += expense.amount;
